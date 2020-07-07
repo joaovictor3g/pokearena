5e267ebe578b7ce88ff7da1456870f903d5e0f57
joaovictor3g <jvdias79797@gmail.com>
2020-07-06 22:46:12 -0300

    :tada: delete interface done

---
+++ web/src/components/DeletePokemon/index.tsx
@@ -0,0 +1,38 @@
+import React, { useState, MouseEvent, SetStateAction, Dispatch } from 'react';
+import api from '../../services/api';
+import './styles.css';
+
+interface Props {
+    id_pokemon: number,
+    id_trainer: number,
+    name: string,
+    onClose: () => void
+}
+
+const DeletePokemon: React.FC<Props> = ({ id_pokemon, id_trainer, name, onClose }) => {
+    
+    async function deletePokemon(e: MouseEvent<HTMLButtonElement>) {
+        try { 
+            const response = await api.delete(`/delete?id_trainer=${id_trainer}&id_pokemon=${id_pokemon}`);
+
+            if(!response.data) {
+                alert('Exclusão não concluída');
+                onClose();
+                return;
+            }
+            alert('Exclusão com sucesso');
+            onClose();
+            
+        } catch(err) {}
+        
+    }
+
+    return (
+        <div className="delete-modal">
+            <span>Tem certeza que quer excluir {name} da lista?</span>
+            <button type="button" className="btn" onClick={deletePokemon}>Exluir Pokemon</button>
+        </div>
+    );
+}
+
+export default DeletePokemon;
--- web/src/pages/SeePokemonCaughtes/index.tsx
+++ web/src/pages/SeePokemonCaughtes/index.tsx
@@ -4,6 +4,8 @@ import id_trainer from '../../utils/id_trainer';
 import { FiTrash2, FiPower } from 'react-icons/fi';
 
 import { Link } from 'react-router-dom';
+
+import DeletePokemon from '../../components/DeletePokemon';
 import './styles.css';
 
 interface Props { 
@@ -15,13 +17,25 @@ interface Props {
 
 const SeePokemonCaughtes: React.FC = () => {
     const [pokemons, setPokemons] = useState<[]>([]);
-    const id = sessionStorage.getItem('id_trainer');
+
+    const id = Number(sessionStorage.getItem('id_trainer'));
+    const [isdeleted, setDelete] = useState<boolean>(false);
+
+    const [idPokemon, setIdPokemon] = useState<number>(0);
+    const [namePokemon, setNamePokemon] = useState<string>('');
 
     useEffect(() => {
         api.get(`/see-your-pokemons/${id}`)
             .then(res => setPokemons(res.data))
 
     }, [pokemons, id]);
+
+    function handleDeletePokemon(id_pokemon: number, name: string) {
+        setDelete(true);
+
+        setIdPokemon(id_pokemon);
+        setNamePokemon(name);
+    }
 
     return (
         <>
@@ -33,13 +47,22 @@ const SeePokemonCaughtes: React.FC = () => {
                     <div key={pokemon.id_pokemon} className="content-pokemons">
                         <div className="trash-and-id">
                             <p className="id">{pokemon.id_pokemon}</p>
-                            <button>
+                            <button onClick={() => handleDeletePokemon(pokemon.id_pokemon, pokemon.name)}>
                                 <FiTrash2 size={22} color="#8c8c8c"/>
                             </button>
                         </div>
                         <p className="name">{pokemon.name}</p>
                         <img src={pokemon.image} alt="pokemon" />
                         <span className="description">{pokemon.description}</span>
+
+                        {isdeleted ? 
+                            <DeletePokemon 
+                                name={namePokemon} 
+                                id_pokemon={idPokemon} 
+                                id_trainer={id} 
+                                onClose={() => setDelete(false)}
+                            /> : 
+                        null}
                   </div>
                 ))}
             </div>
