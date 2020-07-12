c0ad7fe9298c7f18ac9d6411e354c441a1e5efe9
joaovictor3g <jvdias79797@gmail.com>
2020-07-11 23:03:21 -0300

    feat: new page added

--- web/src/pages/ListTrainers/index.tsx
+++ web/src/pages/ListTrainers/index.tsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import api from '../../services/api';
 import './styles.css';
 
+interface Props { 
+    id_trainer: number, 
+    name: string, 
+    image: string 
+}
+
 const ListUsers: React.FC = () => {
+    const [allTrainers, setAllTrainers] = useState<[]>([]);
+
+    useEffect(() => {
+        api.get('get-all-trainers')
+            .then(res => setAllTrainers(res.data));
+    }, []);
+
     return (
-        <div>Hello</div>
+        <div>
+            {allTrainers.map((trainer: Props) => (
+                <div key={trainer.id_trainer}>
+                    <p >{trainer.name}</p>
+                    <img src={`http://localhost:3333/uploads/${trainer.image}`} alt="trainer" />
+                </div>
+            ))}
+        </div>
     );
 };
 
